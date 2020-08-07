a75875b48e60248b442e3a3f67e525946c72c9d7
Josue Rodriguez <jr2589213@gmail.com>
2020-08-07 08:49:00 -0700

    adds solution for ex3 using cache for nums that intersect lists

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,7 +4,34 @@ def intersection(arrays):
     """
     # Your code here
 
+    # count the length of the array in order to
+    # keep track of how many lists there are and
+    # how many intersections we need to be considered truthy
+    arr_len = len(arrays)
+    result = []
+
+    # create empty dict
+    cache = {}
+
+    # loop the the list of arrays
+    for arr in arrays:
+        # loop inside each array
+        for num in arr:
+            # if num is in the cache + 1
+            if num in cache:
+                cache[num] += 1
+            else:
+                # else insert it and start it off at 1
+                cache[num] = 1
+
+    for pair in cache.items():
+        if pair[1] >= arr_len:
+            result.append(pair[0])
+
     return result
+
+
+# print(intersection([[1, 2, 4, 6], [1, 7, 8, 3, 2, 4], [1, 5, 6, 8, 3, 2]]))
 
 
 if __name__ == "__main__":
