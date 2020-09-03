a23511c5dd60aa4b6279dbd11200b5ef99d7a2b8
Josue Rodriguez <jr2589213@gmail.com>
2020-09-03 13:53:02 -0700

    deletes comments and prints in ex2.py file

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,35 +6,21 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
     cache = {}
     travel_order = []
 
     for item in tickets:
         cache[item.source] = item.destination
 
-    print(cache)
-
     begin_value = 'NONE'
-    print(begin_value)
     loop = True
 
     while loop:
-        # get the value of the 'None' key
-
-        # tell it to search for the value of none
         travel_order.append(cache[begin_value])
 
         begin_value = cache[begin_value]
 
         if begin_value == 'NONE':
             return travel_order
-        print(f"travel_order in the while:{travel_order}")
-
-    # if the value is none then return / stop
-    print(travel_order)
 
     return 'hey'
